902b75b54ea83c262cd11243f1de6af9f6124185
Gvyno <gvidaskulikauskas@gmail.com>
2025-04-25 01:05:12 +0300

    Added 2 new Ice Caves enemies
    Added 2 new Ice Caves enemies. Subject to balancing changes.

--- sigmiukonuotikiaigodot/Bars/health_bar.gd
+++ sigmiukonuotikiaigodot/Bars/health_bar.gd
@@ -86,3 +86,27 @@ func _on_slimer_health_changed(new_health: Variant, new_min_health: Variant, new
 	if health_label != null:
 		health_label.text=(str(new_health,"/",new_max_health))
 	pass # Replace with function body.
+
+
+func _on_ice_mimic_health_changed(new_health: Variant, new_min_health: Variant, new_max_health: Variant) -> void:
+	print("AUCH")
+	self.value=new_health
+	self.max_health=new_max_health
+	self.min_health=new_min_health
+	self.min_value=new_min_health
+	self.max_value=new_max_health
+	if health_label != null:
+		health_label.text=(str(new_health,"/",new_max_health))
+	pass # Replace with function body.
+
+
+func _on_ice_wisp_health_changed(new_health: Variant, new_min_health: Variant, new_max_health: Variant) -> void:
+	print("AUCH")
+	self.value=new_health
+	self.max_health=new_max_health
+	self.min_health=new_min_health
+	self.min_value=new_min_health
+	self.max_value=new_max_health
+	if health_label != null:
+		health_label.text=(str(new_health,"/",new_max_health))
+	pass # Replace with function body.
