12b33bf5a35380f0c5699e4f2f15468f1c8f3084
agurkas-morka <97970648+agurkas-morka@users.noreply.github.com>
2025-04-26 20:18:58 +0300

    Made ice cave backgrounds, not yet implemented in ince cave world.

---
+++ sigmiukonuotikiaigodot/Backgrounds/Ice_backgrounds/Scenes/ice_cave_to_outside_change_area.gd
@@ -0,0 +1,29 @@
+extends Area2D
+
+# Hardcoding the scene tree path for the background groups
+var outside_group : ParallaxBackground
+var cave_group : ParallaxBackground
+@export var collision_size : Vector2 = Vector2(100, 50) # Default size
+@onready var collision_shape = $CollisionShape2D
+
+func _ready():
+	
+	# Get the nodes directly from the scene tree (hardcoding paths)
+	outside_group = get_tree().get_nodes_in_group("ice")[0]
+	cave_group = get_tree().get_nodes_in_group("ice")[1]
+	cave_group.visible = false;
+	outside_group.visible = true;
+	
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "Sigmiukas" and cave_group.visible == true :
+		BackgroundFade.transition()
+		await BackgroundFade.on_trasition_finished
+		outside_group.visible = true
+		cave_group.visible = false
+	elif body.name == "Sigmiukas" and outside_group.visible == true :
+		BackgroundFade.transition()
+		await BackgroundFade.on_trasition_finished
+		outside_group.visible = false
+		cave_group.visible = true
+		
