374cc89873290af8ee33e73ae835064eff4e26a9
PandziusG <opsidijienas@gmail.com>
2025-05-02 05:15:47 +0300

    Added incentive items for people
    Added mana,health,manahealth heart for player to get and increase maximum values

---
+++ sigmiukonuotikiaigodot/Food/heart_item.gd
@@ -0,0 +1,35 @@
+extends Area2D
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var food_img: Sprite2D = $food_img
+#@onready var animation = $AnimationPlayer
+signal update_health()
+signal update_mana()
+
+
+var player_in_range := false
+var consumed = false
+
+func _ready():
+	connect("body_entered", _on_body_entered)
+	connect("body_exited", _on_body_exited)
+	#animation.play("Dogy")
+
+func _on_body_entered(body):
+	if body.is_in_group("player") and not consumed:
+		consumed = true
+		if body.health + 20 >= body.max_health+20:
+			body.max_health=body.max_health+20
+			body.health = body.max_health
+		else:
+			body.max_health=body.max_health+20
+			body.health += 20
+		food_img.visible=false
+		collision_shape_2d.disabled=true
+		collision_shape_2d.visible=false
+	emit_signal("update_health")
+
+func _on_body_exited(body):
+	if body.is_in_group("player"):  
+		player_in_range = false
+		if DialogueManager.is_dialogue_active:
+			DialogueManager.end_dialogue()
---
+++ sigmiukonuotikiaigodot/Food/manaitem.gd
@@ -0,0 +1,35 @@
+extends Area2D
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var food_img: Sprite2D = $food_img
+#@onready var animation = $AnimationPlayer
+signal update_mana()
+signal update_health()
+
+
+var player_in_range := false
+var consumed = false
+
+func _ready():
+	connect("body_entered", _on_body_entered)
+	connect("body_exited", _on_body_exited)
+	#animation.play("Dogy")
+
+func _on_body_entered(body):
+	if body.is_in_group("player") and not consumed:
+		consumed = true
+		if body.mana + 20 >= body.max_mana+20:
+			body.max_mana=body.max_mana+20
+			body.mana = body.max_mana
+		else:
+			body.max_mana=body.max_mana+20
+			body.mana += 20
+		food_img.visible=false
+		collision_shape_2d.disabled=true
+		collision_shape_2d.visible=false
+	emit_signal("update_mana")
+
+func _on_body_exited(body):
+	if body.is_in_group("player"):  
+		player_in_range = false
+		if DialogueManager.is_dialogue_active:
+			DialogueManager.end_dialogue()
