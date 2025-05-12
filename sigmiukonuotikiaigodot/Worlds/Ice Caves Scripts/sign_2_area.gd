ba7f14bd75ec7d7b83ca8e4f57913d1fe7d9ff72
PandziusG <opsidijienas@gmail.com>
2025-05-12 19:49:20 +0300

    Sign display text on area entered hofix

--- sigmiukonuotikiaigodot/Worlds/Ice Caves Scripts/sign_2_area.gd
+++ sigmiukonuotikiaigodot/Worlds/Ice Caves Scripts/sign_2_area.gd
@@ -14,19 +14,14 @@ const lines: Array[String] = [
 var player_in_range := false
 
 func _ready():
-	connect("body_entered", _on_body_entered)
-	connect("body_exited", _on_body_exited)
-
-func _process(delta):
-	if player_in_range and Input.is_action_just_pressed("interact"): 
-		DialogueManager.start_dialogue(global_position, lines)
+#	connect("body_entered", _on_body_entered)
+	pass
 
 func _on_body_entered(body):
 	if body.is_in_group("player"):  
-		player_in_range = true
+		DialogueManager.start_dialogue(global_position, lines)
 
 func _on_body_exited(body):
 	if body.is_in_group("player"):  
-		player_in_range = false
 		if DialogueManager.is_dialogue_active:
 			DialogueManager.end_dialogue()
