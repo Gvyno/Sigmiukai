c8d612b906812cbf7da847c3004da3f6e003a5e6
PandziusG <opsidijienas@gmail.com>
2025-05-12 18:28:18 +0300

    Hotfix with world transitions

--- sigmiukonuotikiaigodot/World Transitions/worldChange.gd
+++ sigmiukonuotikiaigodot/World Transitions/worldChange.gd
@@ -1,40 +1,46 @@
 extends Area2D
 var Collision=true
+var Transitioning=false
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Sigmiukas" and body.has_method("save_player_data"):
 		var current_scene_file = get_tree().current_scene.scene_file_path
 		print("Current scene path: ", current_scene_file)
-
-		match current_scene_file:
-			"res://Worlds/Sigmiukas Residence.tscn":
-				body.getabilitycast()
-				body.save_player_data()
-				TransitionScene.transition()
-				await TransitionScene.on_trasition_finished
-				get_tree().change_scene_to_file("res://Worlds/Dark Forest.tscn")
-			"res://Worlds/Dark Forest.tscn":
-				if(BossesState.forest_boss_dead):
-					body.getabilitydoublejump()
+		Transitioning=true
+		if(Transitioning):
+			match current_scene_file:
+				
+				"res://Worlds/Sigmiukas Residence.tscn":
+					$Timer.stop()
+					body.getabilitycast()
+					body.save_player_data()
 					TransitionScene.transition()
 					await TransitionScene.on_trasition_finished
-					body.save_player_data() 
-					get_tree().change_scene_to_file("res://Worlds/Ice Caves.tscn")
-				else:
-					print("BOSS IS NOT DEAD")
-					if(body.name == "Sigmiukas"):
-						body.apply_knockback("left",500);
-			"res://Worlds/Ice Caves.tscn":
-							if(BossesState.ice_caves_boss_dead):
-								body.getabilitycast()
-								TransitionScene.transition()
-								await TransitionScene.on_trasition_finished
-								body.save_player_data() 
-								get_tree().change_scene_to_file("res://Worlds/Dragon's Lair.tscn")
-							else:
-								if(body.name == "Sigmiukas"):
-									body.apply_knockback("left",500);
-			_:
-				print("No matching scene found or already at the last level.")
+					get_tree().change_scene_to_file("res://Worlds/Dark Forest.tscn")
+				"res://Worlds/Dark Forest.tscn":
+					if(BossesState.forest_boss_dead):
+						$Timer.stop()
+						body.getabilitydoublejump()
+						TransitionScene.transition()
+						await TransitionScene.on_trasition_finished
+						body.save_player_data() 
+						get_tree().change_scene_to_file("res://Worlds/Ice Caves.tscn")
+					else:
+						print("BOSS IS NOT DEAD")
+						if(body.name == "Sigmiukas"):
+							body.apply_knockback("left",500);
+				"res://Worlds/Ice Caves.tscn":
+								if(BossesState.ice_caves_boss_dead):
+									$Timer.stop()
+									body.getabilitycast()
+									TransitionScene.transition()
+									await TransitionScene.on_trasition_finished
+									body.save_player_data() 
+									get_tree().change_scene_to_file("res://Worlds/Dragon's Lair.tscn")
+								else:
+									if(body.name == "Sigmiukas"):
+										body.apply_knockback("left",500);
+				_:
+					print("No matching scene found or already at the last level.")
 	else:
 		print("world change u no player wthhhhhh")
 
